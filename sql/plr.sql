c119ef45eb730e30d8a2e4a71eff87920337ee4c
Joe Conway <mail@joeconway.com>
2011-08-18 15:27:32 -0700

    - modest regression test for WINDOW functions

--- sql/plr.sql
+++ sql/plr.sql
@@ -388,3 +388,37 @@ returns setof record as '
 ' language 'plr';
 
 select * from restore_df((select test_serialize('select oid, typname from pg_type where typname in (''oid'',''name'',''int4'')'))) as t(oid oid, typname name);
+
+--Test WINDOW functions
+-- create test table
+CREATE TABLE test_data (
+  fyear integer,
+  firm float8,
+  eps float8
+);
+
+-- insert data for test
+INSERT INTO test_data
+SELECT (b.f + 1) % 10 + 2000 AS fyear,
+	floor((b.f+1)/10) + 50 AS firm,
+       f::float8/100 AS eps
+FROM generate_series(-200,199,1) b(f);
+
+CREATE OR REPLACE
+FUNCTION r_regr_slope(float8, float8)
+RETURNS float8 AS
+$BODY$
+  slope <- NA
+  y <- farg1
+  x <- farg2 
+  if (fnumrows==9) try (slope <- lm(y ~ x)$coefficients[2])
+  return(slope)
+$BODY$
+LANGUAGE plr WINDOW;
+
+SELECT *, r_regr_slope(eps, lag_eps) OVER w AS slope_R
+FROM (SELECT firm, fyear, eps,
+  lag(eps) OVER (ORDER BY firm, fyear) AS lag_eps
+FROM test_data) AS a
+WHERE eps IS NOT NULL
+WINDOW w AS (ORDER BY firm, fyear ROWS 8 PRECEDING);
